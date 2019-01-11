c7d0e1b783ea57bce9123c881af013670642c2c4
JairoReyes <homero_684@hotmail.com>
2019-01-11 11:28:05 -0600

    quedo login, probando (Y)x2

--- app/src/main/java/local/isaac/tt_2018_a031/rest/APIInterface.java
+++ app/src/main/java/local/isaac/tt_2018_a031/rest/APIInterface.java
@@ -9,7 +9,7 @@ import retrofit2.http.POST;
 public interface APIInterface {
 
 
-    @POST("iniciarViaje")
+    @POST("iniciarSesion")
     Call<LoginPDO> loginTerminal(@Body LoginRequest loginRequest);
 
 }
